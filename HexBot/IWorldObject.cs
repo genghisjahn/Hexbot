fd882f0dad90e8cf289f0b258cb64b22c14ed67b
Jon M. Wear <jon.m.wear@gmail.com>
2013-07-02 21:24:44 -0400

    Adding interface to allow objects to be drawn.
    Right now I'm just using robots, but I may want to add other stuff in
    the future and I need a common interface to allow them to be drawn on
    the board.

---
+++ HexBot/IWorldObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexBot
+{
+    public interface IWorldObject
+    {
+        System.Drawing.Color GetColor();
+        System.Drawing.SolidBrush GetShape();
+        
+    }
+}
